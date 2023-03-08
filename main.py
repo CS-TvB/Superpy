55efbb2afda35519942ed7948243673fe84e9b02
Thomas van Boom <38430385+CS-TvB@users.noreply.github.com>
2023-03-08 10:38:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import argparse
 import csv
 import sys
+import os
 from datetime import timedelta, datetime, date
 import matplotlib.pyplot as plt
 
@@ -19,19 +20,14 @@ def set_today():
       sys.stdout.write(atm)
    return atm
 
-#this function increases the time by a positive or negative number and saves the date in "Date.txt".
+#this function sets the date to a new one that is givin and saves the date in "Date.txt".
 def set_advance_time(increase):
-   Today = str(date.today())
-   Todays_format = datetime.strptime(Today, '%Y-%m-%d')
-   increased_day = Todays_format + timedelta(days = increase)
-   Today = increased_day.strftime('%Y-%m-%d')
-   file = open("Date.txt", "w")
-   file.write(Today)
-   file.close()
-   sys.stdout.write("""
-   =====================================================
-   (ᵔᴥᵔ) Time has succsesfully advanced to {}
-   =====================================================\n""".format(Today))
+   increase = datetime.strptime(increase, '%Y-%m-%d')
+   increase = increase.strftime('%Y-%m-%d')
+   with open("Date.txt", "w") as f:
+      f.write(increase)
+      print(f"date updated with date: {increase}")
+   return increase
 
 #this function reads the date stored inside the Date.txt file.
 def read_today():
@@ -43,34 +39,56 @@ def read_today():
    return date
 
 #this function (if all the arguments are correct in main) adds a product to bought.csv.
-def purchase_product(id, product_name, price, buy_date, expiry_date):
-   with open('bought.csv', mode='a', newline='') as file:
+def purchase_product(product_name, price, buy_date, expiry_date):
+   if not os.path.exists("bought.csv"):
+      open("bought.csv", "w").close()
+
+   with open('bought.csv', "r", encoding="utf-8", errors="ignore") as file:
+      lines = file.readlines()
+      last_id = 0
+      if lines:
+         final_line = lines[-1]
+         last_id = str(int(final_line.split(",")[0]) + 1)
+
+   with open('bought.csv', mode='a', newline='') as file:   
       writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-      writer.writerow([id, product_name, price, buy_date, expiry_date])
+      writer.writerow([last_id, product_name, price, buy_date, expiry_date])
    sys.stdout.write("""
-   =====================================================
-   (• ε •) Purchase has succesfully been placed!(• ε •)
-   =====================================================\n""")
+   =====================================
+   Purchase has succesfully been placed!
+   =====================================\n""")
 
 #checks if we ever bought product, and adds it to the sold list if it is there.
-def sell_product(id, name, price, atm):
+def sell_product(name, price, atm):
+   #if the sold file does not exist, create it.
+   if not os.path.exists("sold.csv"):
+      open("sold.csv", "w").close()
+
+#checks if the product is in the bought list.
    with open('bought.csv', 'r') as read_file:
       reader = csv.reader(read_file, delimiter=',')
-      items = [row for row in reader]
-      product = next((item for item in items if item[1] == name), None)
-   if not product:
-      sys.stdout.write("""
-   =======================================================================================================
-   1.. 2.. 3.. 4.. How manny thiefs are in the store? we dont have this on the shelves anymore. ¯\_(ツ)_/¯
-   =======================================================================================================\n""")
-   else:
-      with open('sold.csv', 'a', newline='') as file:
-         writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-         writer.writerow([id, product[0], atm, price])
-         sys.stdout.write("""
-   ============================================
-   The item has succesfully been sold ٩(◕‿◕)۶
-   ============================================\n""")
+      products = []
+      for row in reader:
+         if row[1] == name:
+            products.append(row)
+
+#checks if the product is in the sold list.
+   with open('sold.csv', 'r') as file:
+      file_reader = csv.reader(file, delimiter=',')
+      product = []
+      for rows in file_reader:
+         if rows[1] == name:
+            product.append(rows)
+
+#checks if the product is in the bought list, and if it is in the sold list, if its not, it adds it to the sold list.
+   if products:
+      if product:
+         print("product already sold")
+      else:
+         with open('sold.csv', mode='a', newline='') as file:
+            writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([products[0][0], products[0][1], atm, price])
+         print("product succesfully sold")
 
 #reports the revenue of a day.
 def report_revenue(the_day):
@@ -87,28 +105,31 @@ def report_revenue(the_day):
 def report_profit(gifted_date):
    cost = 0
    revenue = 0
-   with open('bought.csv', mode='r') as csv_file:
-      csv_reader = csv.reader(csv_file, delimiter=',')
-      for row in csv_reader:
-         if row[3] == str(gifted_date):
-            cost += float(row[2])
+
    with open('sold.csv', mode='r') as csvfile:
       csvreader = csv.reader(csvfile, delimiter=',')
       for row in csvreader:
          if row[2] == str(gifted_date):
             revenue += float(row[3])
+
+   with open('bought.csv', mode='r') as csv_file:
+      csv_reader = csv.reader(csv_file, delimiter=',')
+      for row_2 in csv_reader:
+         if row_2[0] == row[0]:
+            cost += float(row_2[2])
+
    profit = revenue - cost
    print ("\nthe profit was", profit)
    return profit
 
 #exports data to csv.
-def export_data_to_CSV(search_string):
+def export_data_to_CSV(date_of_export):
    with open("bought.csv", "r") as original_file:
       with open("filtered_bought.csv", "w", newline='') as filtered_file:
          reader = csv.reader(original_file)
          writer = csv.writer(filtered_file)
          for row in reader:
-            if row[0] == search_string:
+            if str(row[3]) == str(date_of_export):
                writer.writerow(row)
    print("the file can be found in the software folder called, filtered_bought.csv")
    return "filtered_bought.csv"
@@ -123,7 +144,7 @@ def special_function_1(today):
             revenue.append(float(row[3]))
    if revenue:  
       fig, ax = plt.subplots()
-      ax.bar([0], revenue)
+      ax.bar(today.strftime('%Y-%m-%d'), revenue)
       ax.set_xlabel("Day")
       ax.set_ylabel("Revenue (in euro's)")
       ax.set_title(f"Revenue for a single day, {today}")
@@ -142,7 +163,7 @@ def special_function_2(today):
             buying_costs.append(float(row[2]))
    if buying_costs:  
       fig, ax = plt.subplots()
-      ax.bar([0], buying_costs)
+      ax.bar(today.strftime('%Y-%m-%d'), buying_costs)
       ax.set_xlabel("Day")
       ax.set_ylabel("cost (in euro's)")
       ax.set_title(f"bought costs for a single day, {today}")
@@ -157,7 +178,7 @@ def main():
    group = parser.add_mutually_exclusive_group()
    
    group.add_argument('--st', help='Set todays date', action='store_true')
-   group.add_argument('--sat', help='Increase todays date by the giving number of days', type=int)
+   group.add_argument('--sat', help='Set the date to a desired date', action='store_true')
    group.add_argument('--rd', help='Reads the set date', action='store_true')
    group.add_argument('--pp', help='purchase a product', action='store_true')
    group.add_argument('--sp', help ='sell a product', action='store_true')
@@ -172,19 +193,15 @@ def main():
    if args.st:
       set_today()
 
-   if args.sat:
-      set_advance_time(args.sat)
+   while args.sat:
+      increase = input("Please enter the desired date in this format, %Y-%m-%d: ")
+      set_advance_time(increase)
+      break
 
    if args.rd:
       read_today()
 
    while args.pp:
-      id = input("Please enter the product id (must be a 4-digit number): ")
-      if id.isdigit() and len(id) == 4:
-         print("Input accepted.")
-      else:
-         print("Invalid input")
-         break
       product_name = input("Please enter the product name: ")
       if isinstance(product_name, str):
          print("Input accepted.")
@@ -214,17 +231,12 @@ def main():
       except ValueError:
          print("Invalid date format. Please enter date in the format of YYYY-MM-DD.")
          break
-      purchase_product(id, product_name, price, buy_date, expiry_date)
+      purchase_product(product_name, price, buy_date, expiry_date)
       break
 
    while args.sp:
-      id = input("Please enter the product id (must be a 4-digit number): ")
-      if id.isdigit() and len(id) == 4:
-         print("Input accepted.")
-      else:
-         print("Invalid input")
-         break
       name = input("Please enter the product name: ")
+      date_of_today = str(date.today())
       if isinstance(name, str):
          print("Input accepted.")
       else:
@@ -237,7 +249,7 @@ def main():
       except ValueError:
          print("Invalid input")
          break
-      sell_product(id, name, price, set_today())
+      sell_product(name, price, date_of_today)
       break
 
    while args.rr:
@@ -265,13 +277,15 @@ def main():
          break
 
    while args.ebp:
-      ebp = input("Please enter the product id (must be a 4-digit number): ")
-      if ebp.isdigit() and len(ebp) == 4:
-         print("Input accepted.")
-         export_data_to_CSV(ebp)
+      date_of_export = input("Please enter the date you would like to export the bought info from, %Y-%m-%d: ")
+      try:
+         date_of_export = datetime.strptime(date_of_export, '%Y-%m-%d')
+         date_of_export = datetime.date(date_of_export)
+         print("Valid date")
+         export_data_to_CSV(date_of_export)
          break
-      else:
-         print("Invalid input")
+      except ValueError:
+         print("Invalid date format. Please enter date in the format of YYYY-MM-DD.")
          break
 
    while args.sf1:
